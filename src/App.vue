5346a75e42a76902e7bf11d537908984a2c76d05
ekorahy <ekorahy@gmail.com>
2025-02-20 04:08:38 +0700

    Step 7: Learn list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <script setup>
 import { ref } from 'vue'
 
-const awesome = ref(true)
+let id = 0
 
-function toggle() {
-  awesome.value = !awesome.value
+const newTodo = ref('')
+
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' },
+])
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((todoItem) => todoItem.id !== todo.id)
 }
 </script>
 
 <template>
-  <button @click="toggle">Toggle</button>
-  <p v-if="awesome">Vue is awesome!</p>
-  <p v-else>Oh no😢</p>
+  <form @submit.prevent="addTodo">
+    <input type="text" v-model="newTodo" required placeholder="new todo" />
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
