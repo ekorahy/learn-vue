32b80891e634d334550898c2fda1d46e5d2a6d31
ekorahy <ekorahy@gmail.com>
2025-02-20 01:54:33 +0700

    Step 2: Learn declarative rendering

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+const counter = reactive({
+  count: 0,
+})
+counter.count++
+
+const message = ref('Hello World!')
+</script>
+
 <template>
-  <h1>Hello world!</h1>
+  <h1>{{ message.split('').reverse().join('') }}</h1>
+  <span>{{ counter.count }}</span>
 </template>
